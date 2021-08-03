a0dca9fd35980a82d3296c35a87dbca1e6740607
sinaaghli <sina.aghli@gmail.com>
2021-08-03 12:38:58 -0600

    testing with protocol

--- example_internal/main.cpp
+++ example_internal/main.cpp
@@ -2,7 +2,9 @@
 #include <moteus_cpp/mjbots/moteus/moteus_protocol.h>
 #include <moteus_cpp/serial-lib.h>
 #include <chrono>
+#include <iomanip>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <thread>
 
@@ -12,28 +14,60 @@ using namespace std::chrono;       // nanoseconds, system_clock, seconds
 using namespace std;
 
 int main() {
-  mjbots::moteus::QueryCommand q_com;
+  mjbots::moteus::PositionCommand p_com;
+  p_com.position = std::numeric_limits<double>::quiet_NaN();
+  p_com.velocity = 0.2;
+  p_com.maximum_torque = 0.3;
+  p_com.stop_position = 0.8;
 
   mjbots::moteus::CanFrame frame;
   cout << "size was " << (int)frame.size << endl;
   mjbots::moteus::WriteCanFrame wcan_frame(&frame);
-  mjbots::moteus::EmitQueryCommand(&wcan_frame, q_com);
+  mjbots::moteus::PositionResolution pres;
+  pres.kd_scale = mjbots::moteus::Resolution::kIgnore;
+  pres.kp_scale = mjbots::moteus::Resolution::kIgnore;
+  pres.feedforward_torque = mjbots::moteus::Resolution::kIgnore;
+  pres.maximum_torque = mjbots::moteus::Resolution::kIgnore;
+  pres.stop_position = mjbots::moteus::Resolution::kIgnore;
+  pres.velocity = mjbots::moteus::Resolution::kIgnore;
+  pres.watchdog_timeout = mjbots::moteus::Resolution::kIgnore;
+
+  mjbots::moteus::EmitPositionCommand(&wcan_frame, p_com, pres);
   cout << "size is " << (int)frame.size << endl;
-  for (int ii = 0; ii < frame.size; ii++) {
+  for (int ii = 0; ii < (int)frame.size; ii++) {
     cout << (int)frame.data[ii] << ",";
   }
   cout << endl;
+  // mjbots::moteus::QueryCommand q_com;
+  // mjbots::moteus::CanFrame frame;
+  // cout << "size was " << (int)frame.size << endl;
+  // mjbots::moteus::WriteCanFrame wcan_frame(&frame);
+  // mjbots::moteus::EmitQueryCommand(&wcan_frame, q_com);
+  // cout << "size is " << (int)frame.size << endl;
+  // for (int ii = 0; ii < frame.size; ii++) {
+  //   cout << (int)frame.data[ii] << ",";
+  // }
+  // cout << endl;
   const char* port = "/dev/serial/by-id/usb-mjbots_fdcanusb_BE6118CD-if00";
   seriallib serial;
   int fd = serial.serialport_init(port, 115200);
   // serial.serialport_writebytes(fd, (char*)&(frame.data), frame.size);
-  cout << "fd is " << fd << endl;
-  string sdata("can send 8001 420120\r");
-  // string sdata("can status\r");
-  cout << sdata.size() << endl;
-  for (int ii = 0; ii < sdata.size(); ii++) {
-    cout << (int)sdata[ii] << ",";
+
+  // string sdata("can send 8001 420120\r");
+  // string sdata("can send 8001 1010133200063\r");
+
+  stringstream ss;
+  ss << "can send 8001 ";
+  for (int ii = 0; ii < (int)frame.size; ii++) {
+    ss << std::setfill('0') << std::setw(2) << std::hex << (int)frame.data[ii];
   }
+  ss << '\n';
+  string sdata(ss.str());
+
+  cout << "string is " << sdata << endl;
+  // for (int ii = 0; ii < sdata.size(); ii++) {
+  //   cout << (int)sdata[ii] << ",";
+  // }
   // while (1) {
   // serial.serialport_writebytes(fd, (char*)&(frame.data), frame.size);
 
