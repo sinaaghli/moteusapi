c0f6bad12b694074ef6e1887b11d880584485352
Sina Aghli <sina@Sinas-MacBook-Pro.local>
2021-08-03 21:17:56 -0600

    added an example with new interface

--- example_internal/main3.cpp
+++ example_internal/main3.cpp
@@ -1,10 +1,20 @@
 #include <moteusapi/MoteusAPI.h>
 
+#include <chrono>
+#include <thread>
+
 int main() {
   string dev_name("/dev/tty.usbmodemBE6118CD1");
   int moteus_id = 1;
   MoteusAPI api(dev_name, moteus_id);
-  api.SendPositionCommand(0.5, 0.2, 0.3);
 
+  api.SendPositionCommand(-1, 0.3, 0.3);
+  this_thread::sleep_for(chrono::milliseconds(3000));
+
+  for (int jj = 0; jj < 3; jj++)
+    for (int ii = -314; ii < 314; ii++) {
+      api.SendPositionCommand(cos(ii * 0.01), 3, 3);
+      this_thread::sleep_for(chrono::milliseconds(3));
+    }
   return 0;
 }
